daef08a51acbd6bc4e6b94a86d5e72a2b030313c
Thirumalai Raja <thirumalai.py@gmail.com>
2025-08-29 12:54:00 +0530

    Update the variable

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,13 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-    region ="ap-south-1"
-} 
+  region = "ap-south-1"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,3 @@
-variable "aws_region" { type = string }
 variable "key_name" { type = string  }
 
 variable "instance_type" {
