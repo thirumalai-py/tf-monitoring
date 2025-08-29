b4032d4f1863a900ff9b30d30e6dbac3766b9046
Thirumalai Raja <thirumalai.py@gmail.com>
2025-08-29 12:35:40 +0530

    Terraform Volume addition

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,11 @@ resource "aws_instance" "web" {
   key_name = var.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
+  root_block_device {
+    volume_size = var.volume_size
+    volume_type = var.volume_type
+  }
+
   tags = {
     Name = var.tag_name
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,6 +9,16 @@ variable "ami_id" {
     type = string
 }
 
+variable "volume_size" {
+  description = "value of volume size in GB"
+  type    = number
+}
+
+variable "volume_type" {
+  description = "value of volume type"
+  type = string
+}
+
 variable "tag_name" {
   description = "value of tag name"
   type    = string
